167098b412549fd4b85e754ab416828185ef247c
frederikagger <frederikagger@hotmail.com>
2020-07-07 14:42:26 +0200

    v-if added on next and previous btn

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -3,7 +3,7 @@
         <nav aria-label="Page navigation">
             <ul class="pagination justify-content-center">
                 <li class="page-item">
-                    <router-link :to="'/hjem/'+previousPage" class="page-link">Previous</router-link>
+                    <router-link :to="'/hjem/'+previousPage" v-if="thisPage != 1" class="page-link">Previous</router-link>
                 </li>
                 <router-link :to="'/hjem/'+index"
                              v-for="index in numberOfPages"
@@ -11,7 +11,7 @@
                              active-class="active"
                              class="page-item"><a class="page-link">{{index}}</a></router-link>
                 <li class="page-item">
-                    <router-link :to="'/hjem/'+nextPage" class="page-link">Next</router-link>
+                    <router-link :to="'/hjem/'+nextPage" v-if="thisPage != numberOfPages" class="page-link">Next</router-link>
                 </li>
             </ul>
         </nav>
@@ -26,17 +26,16 @@
         data() {
             return {
                 thisPage: parseInt(this.$route.params.id),
-                nextPage: this.thisPage+1,
-                previousPage: this.thisPage-1
+                nextPage: parseInt(this.$route.params.id)+1,
+                previousPage: parseInt(this.$route.params.id)-1
             }
         },
         watch: {
             '$route'(to){
-                this.thisPage = parseInt(to.params.id)
-                this.nextPage = this.thisPage+1
-                this.previousPage = this.thisPage-1
+                    this.thisPage = parseInt(to.params.id)
+                    this.nextPage = this.thisPage+1
+                    this.previousPage = this.thisPage-1
             }
-
         }
     }
 </script>
