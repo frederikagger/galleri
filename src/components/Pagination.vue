3158eac835ba82860a7c0d9cc48664ecaabc9cd1
Frede <frederikagger@hotmail.com>
2020-07-13 09:28:33 +0200

    props currentpage added again

--- src/components/Pagination.vue
+++ src/components/Pagination.vue
@@ -3,7 +3,7 @@
         <nav aria-label="Page navigation">
             <ul class="pagination justify-content-center">
                 <li class="page-item">
-                    <router-link :to="'/hjem/'+previousPage" v-if="thisPage != 1" class="page-link">Previous</router-link>
+                    <router-link :to="'/hjem/'+previousPage" v-if="currentPage != 1" class="page-link">Previous</router-link>
                 </li>
                 <router-link :to="'/hjem/'+index"
                              v-for="index in numberOfPages"
@@ -11,7 +11,7 @@
                              active-class="active"
                              class="page-item"><a class="page-link">{{index}}</a></router-link>
                 <li class="page-item">
-                    <router-link :to="'/hjem/'+nextPage" v-if="thisPage != numberOfPages" class="page-link">Next</router-link>
+                    <router-link :to="'/hjem/'+nextPage" v-if="currentPage != numberOfPages" class="page-link">Next</router-link>
                 </li>
             </ul>
         </nav>
@@ -22,19 +22,18 @@
     export default {
         props: {
             numberOfPages: Number,
+            currentPage: Number
         },
         data() {
             return {
-                thisPage: parseInt(this.$route.params.id),
-                nextPage: parseInt(this.$route.params.id)+1,
-                previousPage: parseInt(this.$route.params.id)-1
+                nextPage: this.currentPage+1,
+                previousPage: this.currentPage-1
             }
         },
         watch: {
-            '$route'(to){
-                    this.thisPage = parseInt(to.params.id)
-                    this.nextPage = this.thisPage+1
-                    this.previousPage = this.thisPage-1
+            currentPage() {
+                this.nextPage = this.currentPage+1;
+                this.previousPage = this.currentPage-1
             }
         }
     }
