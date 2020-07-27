0465509976e85081197cdebcd988e677c9a0a728
frederikagger <frederikagger@hotmail.com>
2020-07-27 13:31:26 +0200

    moved http get request in created hook back to paintings.vue

--- src/components/Paintings.vue
+++ src/components/Paintings.vue
@@ -8,46 +8,43 @@
 <script>
     import Painting from "./Painting";
     import Pagination from "./Pagination";
-    import axios from 'axios'
+    import axios from "axios";
 
     export default {
-        created() {
-            axios.get('https://gerdagger-72890.firebaseio.com/paintings.json')
-            .then(res => {
-                const data = res.data;
-                for (let key in data){
-                    const painting = data[key];
-                    this.paintings.push(painting);
-                }
-                console.log(this.paintings)
-            })
-            .catch(error => console.log(error));
-        },
-
         components: {
             appPainting: Painting,
             appPagination: Pagination,
+        },
+        created() {
+            axios.get('https://gerdagger-72890.firebaseio.com/paintings.json')
+                .then(res => {
+                    const data = res.data;
+                    for (let key in data) {
+                        const painting = data[key];
+                        this.paintings.push(painting);
+                    }
+                })
+                .catch(error => console.log(error));
         },
         computed: {
             pages() {
                 return Math.ceil(this.paintings.length / this.pageSize);
             },
             visiblePaintings() {
-                return this.paintings.slice((this.currentPage-1) * this.pageSize,
-                    ((this.currentPage-1) * this.pageSize) + this.pageSize);
+                return this.paintings.slice((this.currentPage - 1) * this.pageSize,
+                    ((this.currentPage - 1) * this.pageSize) + this.pageSize);
             },
         },
         watch: {
-            '$route'(to){
+            '$route'(to) {
                 this.currentPage = parseInt(to.params.id);
             },
-
         },
         data: function () {
             return {
                 currentPage: parseInt(this.$route.params.id),
                 pageSize: 9,
-                paintings: [],
+                paintings: []
             }
         }
     }
