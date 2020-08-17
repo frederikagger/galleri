a3c34ec028ceccf9ab79d122298f82780229b90f
frederikagger <frederikagger@hotmail.com>
2020-08-17 22:04:55 +0200

    formatted file

--- src/components/Admin/Admin.vue
+++ src/components/Admin/Admin.vue
@@ -1,54 +1,58 @@
 <template>
-  <div>
-    <nav class="nav nav-pills justify-content-center nav-fill">
-      <router-link class="mx-md-2 btn-lg btn-primary" exact-active-class="active" :to="{name: 'Upload'}">
-        Upload
-      </router-link>
-      <router-link class="mx-md-2 btn-primary btn-lg" exact-active-class="active" :to="{name: 'Slet'}">
-        Slet
-      </router-link>
-      <button class="mx-md-2 btn-lg btn-danger" v-if="loggedIn" @click="showbox" exact-active-class="active">
-        Logout
-      </button>
-    </nav>
-    <br>
-    <transition name="bounceRight" style="animation-duration: 0.1ms" mode="out-in">
-      <router-view :paintings="paintings"/>
-    </transition>
-  </div>
+    <div>
+        <nav class="nav nav-pills justify-content-center nav-fill">
+            <router-link class="mx-md-2 btn-lg btn-primary" exact-active-class="active" :to="{name: 'Upload'}">
+                Upload
+            </router-link>
+            <router-link class="mx-md-2 btn-primary btn-lg" exact-active-class="active" :to="{name: 'Slet'}">
+                Slet
+            </router-link>
+            <router-link class="mx-md-2 btn-primary btn-lg" exact-active-class="active" :to="{name: 'Alle'}">
+                Indbakke
+            </router-link>
+            <button class="mx-md-2 btn-lg btn-danger" v-if="loggedIn" @click="showbox" exact-active-class="active">
+                Logout
+            </button>
+        </nav>
+        <br>
+        <transition name="bounceRight" style="animation-duration: 0.1ms" mode="out-in">
+            <router-view :paintings="paintings"/>
+        </transition>
+    </div>
 </template>
 
 <script>
-import {auth} from '@/firebase';
+    import {auth} from '@/firebase';
 
-export default {
-  props: {
-    paintings: {
-      type: Array,
-      required: true,
-    },
-  },
-  computed: {
-    loggedIn() {
-      return this.$store.getters.loggedIn;
-    },
-  },
-  methods: {
-    showbox() {
-      this.$bvModal.msgBoxConfirm('Er du sikker på du vil logge ud?').then(value => {
-        if (value) {
-          this.$store.dispatch('logout');
-          auth.signOut();
-          this.$router.replace({name: 'Login'});
-        }
-      }).catch(err => {
-        console.log(err);
-      });
-    },
-  },
-};
+    export default {
+        props: {
+            paintings: {
+                type: Array,
+                required: true,
+            },
+        },
+        computed: {
+            loggedIn() {
+                return this.$store.getters.loggedIn;
+            },
+        },
+        methods: {
+            showbox() {
+                this.$bvModal.msgBoxConfirm('Er du sikker på du vil logge ud?').then(value => {
+                    if (value) {
+                        this.$store.dispatch('logout');
+                        auth.signOut();
+                        this.$router.replace({name: 'Login'});
+                    }
+                }).catch(err => {
+                    console.log(err);
+                });
+            },
+        },
+    };
 </script>
 
 <style scoped>
+
 
 </style>
